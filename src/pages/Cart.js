5a0fe3a62b829fe328d13794640116dbe7a11c3c
yuriipereverziev <yuripereverzev@gmail.com>
2022-12-24 11:46:16 +0200

    fix path pages

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -260,7 +260,7 @@ const Cart = () => {
             </div>
             <div className="cart__bottom">
                 <div className="cart__bottom-details">
-                    <span> Всего пицц: <b>3 шт.</b> </span>
+                    <span> Всего пицц: <b>3 шт.</b></span>
                     <span> Сумма заказа: <b>900 ₽</b> </span>
                 </div>
                 <div className="cart__bottom-buttons">
