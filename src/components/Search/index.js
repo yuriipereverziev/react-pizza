a14753c8ec7fdcde336cf712f608f47b2381d59b
yuriipereverziev <yuripereverzev@gmail.com>
2023-01-12 09:04:33 +0200

    add: block search - hook useCallback

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import styles from './Search.module.scss';
-
+import debounce from 'lodash.debounce'
 import {SearchContext} from "../../App";
 
 const Search = () => {
-    const {searchValue, setSearchValue} = React.useContext(SearchContext)
+    const [value, setValue] = React.useState('')
+    const {setSearchValue} = React.useContext(SearchContext)
     const inputRef = React.useRef()
+
     const onClickClear = () => {
         setSearchValue('')
+        setValue('')
         inputRef.current.focus()
     }
+
+
+    const updateSearchValue = React.useCallback(
+        debounce((str) => {
+            setSearchValue(str)
+        }, 1000),
+        [],
+    )
+
+    const onChangeInput = (event) => {
+        setValue(event.target.value)
+        updateSearchValue(event.target.value)
+    }
+
 
     return (
         <div className={styles.root}>
@@ -48,12 +65,12 @@ const Search = () => {
             </svg>
             <input
                 ref={inputRef}
-                value={searchValue}
-                onChange={(event) => setSearchValue(event.target.value)}
+                value={value}
+                onChange={onChangeInput}
                 className={styles.input}
                 placeholder="Поиск пиццы..."
             />
-            {searchValue && (
+            {value && (
                 <svg
                     onClick={onClickClear}
                     className={styles.clearIcon}
